cb2703a88d7ebf7c46021658a246eda4001fed5a
Elad Shaham <eshaham@paypal.com>
2017-12-24 14:50:24 +0200

    make necessary changes for v0.4

--- src/scrape.js
+++ src/scrape.js
@@ -2,10 +2,10 @@ import moment from 'moment';
 import inquirer from 'inquirer';
 import json2csv from 'json2csv';
 
-import { CONFIG_FOLDER, DOWNLOAD_FOLDER, SCRAPERS } from './definitions';
+import { CONFIG_FOLDER, DOWNLOAD_FOLDER } from './definitions';
 import { writeFile, readJsonFile } from './helpers/files';
 import { decryptCredentials } from './helpers/credentials';
-import { discountScraper, leumiCardScraper, isracardScraper } from './helpers/scrapers';
+import { SCRAPERS, createScraper } from './helpers/scrapers';
 
 async function chooseScraper() {
   const scraperNameResult = await inquirer.prompt([{
@@ -22,33 +22,27 @@ async function chooseScraper() {
   return scraperNameResult.scraperName;
 }
 
-function getScraperByName(scraperName) {
-  switch (scraperName) {
-    case 'discount':
-      return discountScraper;
-    case 'leumiCard':
-      return leumiCardScraper;
-    case 'isracard':
-      return isracardScraper;
-    default:
-      throw new Error(`Unknown scraper ${scraperName}`);
-  }
-}
-
 export default async function () {
   const scraperName = await chooseScraper();
   const encryptedCredentials = await readJsonFile(`${CONFIG_FOLDER}/${scraperName}.json`);
   if (encryptedCredentials) {
     const credentials = decryptCredentials(encryptedCredentials);
     const options = {
-      startDate: moment().startOf('month').subtract(1, 'month').toDate(),
+      companyId: scraperName,
+      startDate: moment().startOf('month').subtract(4, 'month').toDate(),
       verbose: false,
-      eventsCallback: (msg) => {
-        console.log(msg);
-      },
     };
-    const scraper = getScraperByName(scraperName);
-    const result = await scraper(credentials, options);
+    let result;
+    try {
+      const scraper = createScraper(options);
+      scraper.onProgress((companyId, payload) => {
+        console.log(`${companyId}: ${payload.type}`);
+      });
+      result = await scraper.scrape(credentials);
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
     console.log(`success: ${result.success}`);
     if (result.success) {
       console.log(`account #: ${result.accountNumber}`);
@@ -57,16 +51,18 @@ export default async function () {
         return {
           Date: moment(txn.date).format('DD/MM/YYYY'),
           Payee: txn.description,
-          Outflow: txn.amount,
+          Outflow: txn.type !== 'installments' ? txn.chargedAmount : txn.originalAmount,
+          Installment: txn.installments ? txn.installments.number : null,
+          Total: txn.installments ? txn.installments.total : null,
         };
       });
-      const fields = ['Date', 'Payee', 'Outflow'];
+      const fields = ['Date', 'Payee', 'Outflow', 'Installment', 'Total'];
       const csv = json2csv({ data: txns, fields, withBOM: true });
       await writeFile(`${DOWNLOAD_FOLDER}/${scraperName}.csv`, csv);
       console.log('file saved');
     } else {
       console.log(`error type: ${result.errorType}`);
-      console.log(`error message: ${result.errorMessage}`);
+      console.log('error:', result.errorMessage);
     }
   } else {
     console.log('Could not find credentials file');
