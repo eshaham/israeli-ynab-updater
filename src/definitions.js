eee6fe13c3925489a5a202ebb9ad5ddba0cc4685
Elad Shaham <eshaham@paypal.com>
2018-02-11 21:37:53 +0200

    change default target location

--- src/definitions.js
+++ src/definitions.js
@@ -2,4 +2,4 @@ import os from 'os';
 
 export const CONFIG_FOLDER = `${os.homedir()}/.ynab-updater`;
 export const SETTINGS_FILE = `${CONFIG_FOLDER}/settings.json`;
-export const DOWNLOAD_FOLDER = `${os.homedir()}/Downloads/YNAB-Transactions`;
+export const DOWNLOAD_FOLDER = `${os.homedir()}/Downloads/Transactions`;
