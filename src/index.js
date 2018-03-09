2c42762a1315e32cb2d8382b8bbbd6071df9b412
Eran Sakal <eran@sakalim.com>
2018-03-10 00:53:23 +0200

    rebrand scraping as individual scraping and move settings read and write into helpers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import yargs from 'yargs';
-import scrape from './scrape';
+import scrape from './scrape/scrape-individual';
 import setupMainMenu from './setup/setup-main-menu';
 
 const args = yargs.options({
@@ -15,7 +15,7 @@ const args = yargs.options({
   },
 }).help().argv;
 
-if (!args.mode || args.mode === 'scrape') {
+if (!args.mode || args.mode === 'scraping') {
   scrape(args.show);
 } else if (args.mode === 'setup') {
   setupMainMenu();
