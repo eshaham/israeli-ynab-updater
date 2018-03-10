74e3aa01405ef416fe5b7c8dad3ff40c79fd00ea
Eran Sakal <eran@sakalim.com>
2018-03-10 15:32:57 +0200

    simplify setup main menu inquirer prompt implementation

--- src/setup/setup-main-menu.js
+++ src/setup/setup-main-menu.js
@@ -2,19 +2,24 @@ import inquirer from 'inquirer';
 
 import setupScrapers from './setup-scrapers';
 
-
 export default async function () {
-  const { next } = await inquirer.prompt({
+  const { setupType } = await inquirer.prompt({
     type: 'list',
-    name: 'next',
+    name: 'setupType',
     message: 'What would you like to setup?',
     choices: [
       {
-        name: 'Individual Scrapers',
-        value: setupScrapers,
+        name: 'Setup a new scraper',
+        value: 'scraper',
       },
     ],
   });
 
-  await next();
+  switch (setupType) {
+    case 'scraper':
+      setupScrapers();
+      break;
+    default:
+      break;
+  }
 }
